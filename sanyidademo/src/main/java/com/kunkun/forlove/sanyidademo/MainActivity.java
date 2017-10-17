794c69eca37f10202569b15b9fed05eb5d8ad5e5
kun <niuyukunwork@163.com>
2017-10-17 16:57:46 +0800

    修改上传

---
+++ sanyidademo/src/main/java/com/kunkun/forlove/sanyidademo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.kunkun.forlove.sanyidademo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+/*
+* 其中安防工程的是在工程项目中的一个很小的分支,但是安防工程也具有自己的特有的特点,,,,,,,  我们可有使用现有的互联技术和管理技术;来进行我们的管理达到统一的效果.
+*
+* 在规模上各个项目是不一致的;并且各个项目根据自己的特点而需求,本论文中中海公司是作为我们主要的研究对象,来使用系统分析的来获取我们项目开始到结束过程中使用的管理系统.
+*
+* 在项目管理系统分析阶段,我们使用相关的项目管理知识体系和信息化等知识理论，根据在项目管理的侧重点进行分析研究,首先是对项目可行性的分析,主要包括市场上的可行性分析、技术上的可行性分析、经济上可行性分析
+*
+* 本文通过中海公司为主要的研究对象.根据公司现在的运营现状和出现的问题的整理分析和公司在业务上的特殊性,我们们对公司工程项目的运行的需要的的工程项目管理软件提出
+* 系统的需求模型,
+*以及为以后的系统开发提供参考,该项目管理系统包括的主要的模型如下:项目管理、资金 管理、人员管理、合同管理等系统模型.
+*
+*
+*/
+
+
+
+    }
+}
