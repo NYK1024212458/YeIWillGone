5ec520710a9a36857b22442393a24a1e92f473a3
kun <niuyukunwork@163.com>
2017-10-19 16:49:16 +0800

    老师出现错误

--- sanyida/src/main/java/com/kunkun/forlove/sanyidademo/MainActivity.java
+++ sanyida/src/main/java/com/kunkun/forlove/sanyidademo/MainActivity.java
@@ -5,6 +5,9 @@ import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.iflytek.aiui.AIUIAgent;
@@ -14,6 +17,7 @@ import com.iflytek.aiui.AIUIListener;
 import com.iflytek.aiui.AIUIMessage;
 
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -24,46 +28,65 @@ public class MainActivity extends AppCompatActivity {
     private AIUIAgent mAIUIAgent;
 
     private int mAIUIState = AIUIConstant.STATE_IDLE;
+    private EditText mNlpText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mContext = MainActivity.this;
-        // 第一步就是创建 AIUIAgent
-        creatAiuiAgent();
 
+        initView();
 
-        startAiui();
+        if( !checkAIUIAgent() ){
+            return;
+        }
+        startVoiceNlp();
+
 
 
     }
 
-    private void startAiui() {
+    private void initView() {
+        mNlpText = (EditText) findViewById(R.id.tv_shhowtext);
+    }
 
+
+    private boolean checkAIUIAgent(){
+        if( null == mAIUIAgent ){
+            Log.i( TAG, "create aiui agent" );
+            mAIUIAgent = AIUIAgent.createAgent( this, getAIUIParams(), mAIUIListener );
+            AIUIMessage startMsg = new AIUIMessage(AIUIConstant.CMD_START, 0, 0, null, null);
+            mAIUIAgent.sendMessage( startMsg );
+        }
+
+        if( null == mAIUIAgent ){
+            final String strErrorTip = "创建 AIUI Agent 失败！";
+            showTip( strErrorTip );
+            this.mNlpText.setText( strErrorTip );
+        }
+
+        return null != mAIUIAgent;
+    }
+
+    private void startVoiceNlp(){
+        Log.i( TAG, "start voice nlp" );
+        mNlpText.setText("");
 
         // 先发送唤醒消息，改变AIUI内部状态，只有唤醒状态才能接收语音输入
-        if (AIUIConstant.STATE_WORKING != this.mAIUIState) {
+        // 默认为oneshot 模式，即一次唤醒后就进入休眠，如果语音唤醒后，需要进行文本语义，请将改段逻辑copy至startTextNlp()开头处
+        if( AIUIConstant.STATE_WORKING != 	this.mAIUIState ){
             AIUIMessage wakeupMsg = new AIUIMessage(AIUIConstant.CMD_WAKEUP, 0, 0, "", null);
             mAIUIAgent.sendMessage(wakeupMsg);
         }
 
-// 打开AIUI内部录音机，开始录音
+        // 打开AIUI内部录音机，开始录音
         String params = "sample_rate=16000,data_type=audio";
-        AIUIMessage writeMsg = new AIUIMessage(AIUIConstant.CMD_START_RECORD, 0, 0, params, null);
+        AIUIMessage writeMsg = new AIUIMessage( AIUIConstant.CMD_START_RECORD, 0, 0, params, null );
         mAIUIAgent.sendMessage(writeMsg);
-
     }
 
-    private void creatAiuiAgent() {
-        //创建AIUIAgent
-        mAIUIAgent = AIUIAgent.createAgent(mContext, getAIUIParams(), mAIUIListener);
 
-        //发送`CMD_START`消息，使AIUI处于工作状态
-        AIUIMessage startMsg = new AIUIMessage(AIUIConstant.CMD_START, 0, 0, null, null);
-        mAIUIAgent.sendMessage(startMsg);
-
-    }
 
     /**
      * 读取assets目录下的cfg/aiui_phone_user.cfg文件而获得的字符串；
@@ -88,38 +111,7 @@ public class MainActivity extends AppCompatActivity {
         return params;
     }
 
-    AIUIListener mAIUIListener = new AIUIListener() {
-
-        @Override
-        public void onEvent(AIUIEvent event) {
-            switch (event.eventType) {
-                //唤醒事件
-                case AIUIConstant.EVENT_WAKEUP: {
-                    break;
-                }
-                //结果事件（包含听写，语义，离线语法结果）
-                case AIUIConstant.EVENT_RESULT: {
-                    break;
-                }
-                //休眠事件
-                case AIUIConstant.EVENT_SLEEP: {
-                    break;
-                }
-                //错误事件
-                case AIUIConstant.EVENT_ERROR: {
-                    break;
-                }
-            }
-
-
-        }
-    };
-
-
-
-
-
-    private AIUIListener mNAIUIListener = new AIUIListener() {
+    private AIUIListener mAIUIListener = new AIUIListener() {
 
         @Override
         public void onEvent(AIUIEvent event) {
@@ -130,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
                     break;
 
                 case AIUIConstant.EVENT_RESULT: {
-                    //解析结果
+                    Log.i( TAG,  "on event: "+ event.eventType );
                     try {
                         JSONObject bizParamJson = new JSONObject(event.info);
                         JSONObject data = bizParamJson.getJSONArray("data").getJSONObject(0);
@@ -140,9 +132,9 @@ public class MainActivity extends AppCompatActivity {
                         if (content.has("cnt_id")) {
                             String cnt_id = content.getString("cnt_id");
                             JSONObject cntJson = new JSONObject(new String(event.data.getByteArray(cnt_id), "utf-8"));
-/*
+
                             mNlpText.append( "\n" );
-                            mNlpText.append(cntJson.toString());*/
+                            mNlpText.append(cntJson.toString());
 
                             String sub = params.optString("sub");
                             if ("nlp".equals(sub)) {
@@ -153,17 +145,17 @@ public class MainActivity extends AppCompatActivity {
                         }
                     } catch (Throwable e) {
                         e.printStackTrace();
-                       /* mNlpText.append( "\n" );
-                        mNlpText.append( e.getLocalizedMessage() );*/
+                        mNlpText.append( "\n" );
+                        mNlpText.append( e.getLocalizedMessage() );
                     }
 
-                  /*  mNlpText.append( "\n" );*/
+                    mNlpText.append( "\n" );
                 } break;
 
                 case AIUIConstant.EVENT_ERROR: {
                     Log.i( TAG,  "on event: "+ event.eventType );
-                  /*  mNlpText.append( "\n" );
-                    mNlpText.append( "错误: "+event.arg1+"\n"+event.info );*/
+                    mNlpText.append( "\n" );
+                    mNlpText.append( "错误: "+event.arg1+"\n"+event.info );
                 } break;
 
                 case AIUIConstant.EVENT_VAD: {
@@ -186,7 +178,7 @@ public class MainActivity extends AppCompatActivity {
                     showTip("停止录音");
                 } break;
 
-                case AIUIConstant.EVENT_STATE: {    // 状态事件
+                case AIUIConstant.EVENT_STATE: {	// 状态事件
                     mAIUIState = event.arg1;
 
                     if (AIUIConstant.STATE_IDLE == mAIUIState) {
@@ -205,13 +197,15 @@ public class MainActivity extends AppCompatActivity {
                     if( AIUIConstant.CMD_UPLOAD_LEXICON == event.arg1 ){
                         showTip( "上传"+ (0==event.arg2?"成功":"失败") );
                     }
-                } break;
+                }break;
 
                 default:
                     break;
             }
         }
+
     };
+
 
     private void showTip(String str) {
         Toast.makeText(mContext,str,Toast.LENGTH_SHORT).show();
